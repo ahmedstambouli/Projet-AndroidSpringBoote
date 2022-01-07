ad8f0982abefc769ad749fde6e54e71990da5542
labidimalek <melek.islem105@gmail.com>
2022-01-07 01:40:48 +0100

    bottom navigation bar fixed

--- app/src/main/java/com/example/projetintegrationspringboot/Fragmet.java
+++ app/src/main/java/com/example/projetintegrationspringboot/Fragmet.java
@@ -3,6 +3,7 @@ package com.example.projetintegrationspringboot;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
@@ -19,7 +20,10 @@ public class Fragmet extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_fragmet);
-        getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,new ProfilFragment()).commit();
+        ProfilFragment fragment = new ProfilFragment();
+        FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
+        fragmentTransaction.replace(R.id.fragment_container, fragment);
+        fragmentTransaction.commit();
         Objects.requireNonNull(getSupportActionBar()).setBackgroundDrawable(new ColorDrawable(getResources().getColor(R.color.B1)));
 
         btnV=(BottomNavigationView)findViewById(R.id.bottom_navigation);
@@ -27,18 +31,29 @@ public class Fragmet extends AppCompatActivity {
             @Override
             public void onNavigationItemReselected(@NonNull MenuItem item) {
 
-                Fragment temp=null;
-
                 switch (item.getItemId()){
 
-                    case R.id.nav_pers:temp=new ProfilFragment();
+                    case R.id.nav_pers:
+                        ProfilFragment fragment1 = new ProfilFragment();
+                        FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
+                        fragmentTransaction.replace(R.id.fragment_container, fragment1);
+                        fragmentTransaction.commit();
                         break;
-                    case R.id.nav_listestage:temp=new offredestageFragment();
+                    case R.id.nav_listestage:
+                        offredestageFragment fragment2 = new offredestageFragment();
+                        FragmentTransaction fragmentTransaction1 = getSupportFragmentManager().beginTransaction();
+                        fragmentTransaction1.replace(R.id.fragment_container, fragment2);
+                        fragmentTransaction1.commit();
                         break;
-                    case R.id.nav_baskete:temp=new demandedestageFragment();
+                    case R.id.nav_baskete:
+                        demandedestageFragment fragment3 = new demandedestageFragment();
+                        FragmentTransaction fragmentTransaction2 = getSupportFragmentManager().beginTransaction();
+                        fragmentTransaction2.replace(R.id.fragment_container, fragment3);
+                        fragmentTransaction2.commit();
+                        break;
 
                 }
-                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,temp).commit();
+
 
 
             }
