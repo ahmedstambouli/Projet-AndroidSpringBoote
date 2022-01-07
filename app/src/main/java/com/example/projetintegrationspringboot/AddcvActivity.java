1c3ed2981ad4a69c2600946965139988b44149db
labidimalek <melek.islem105@gmail.com>
2022-01-07 19:25:06 +0100

    interface addCV

---
+++ app/src/main/java/com/example/projetintegrationspringboot/AddcvActivity.java
@@ -0,0 +1,16 @@
+package com.example.projetintegrationspringboot;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AddcvActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_addcv);
+
+
+    }
+}
